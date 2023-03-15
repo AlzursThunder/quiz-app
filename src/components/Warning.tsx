84e8e5574815ee07525903a91c18d0f9e1adb12e
Piotr Dziedzina <piotrdziedzina.dev@gmail.com>
2023-03-15 23:26:26 +0100

    Add Warning component and styles

---
+++ src/components/Warning.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from '../styles/styles-components/Warning.module.css'
+
+interface WarningProps {
+	message: string
+}
+
+const Warning = (props: WarningProps) => {
+	const { message } = props
+
+	return (
+		<div className={`alert alert-danger ${styles.message}`} role="alert">
+			<img
+				className={styles["danger-icon"]}
+				src="/danger-icon.png"
+				alt="stop right there"
+			/>
+			<div>{message}</div>
+		</div>
+	);
+}
+
+export default Warning
